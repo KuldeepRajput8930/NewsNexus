1b4970f9b219f018c2cdb0ecb9d1f7321248522c
kuldeepRajput8930 <Kuldeeprajput893046@gmail.com>
2024-02-10 00:12:34 +0530

    Add top loading bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+export default class App extends Component {
+  pageSize = 8;
+  apikey = process.env.REACT_APP_NEWS_API;
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({
+      progress: progress
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+          />
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element=<News apikey={this.apikey} key="general" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="general" /> />
+            <Route exact path="/health" element=<News apikey={this.apikey} key="health" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="health" /> />
+            <Route exact path="/sports" element=<News apikey={this.apikey} key="sports" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="sports" /> />
+            <Route exact path="/science" element=<News apikey={this.apikey} key="science" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="science" /> />
+            <Route exact path="/business" element=<News apikey={this.apikey} key="business" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="business" /> />
+            <Route exact path="/technology" element=<News apikey={this.apikey} key="technology" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="technology" /> />
+            <Route exact path="/entertainment" element=<News apikey={this.apikey} key="entertainment" pageSize={this.pageSize} setProgress={this.setProgress} country="in" category="entertainment" /> />
+          </Routes>
+        </Router >
+      </div >
+    )
+  }
 }
 
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, ImageUrl, NewsUrl, source, author, publishedAt } = this.props;
+    return (
+      <div className="card my-4" style={{ width: '18rem' }}>
+
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-primary" style={{ left: '87%' }}>{source}</span>
+        <img src={ImageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title} ...</h5>
+          <p className="card-text">{description} ...</p>
+          <div className="dateAndAuthor">
+            <p className='paragraph'><small className="text-body-secondary">By <strong>{author}</strong></small></p>
+            <p className='paragraph'><small className="text-body-secondary"><strong>{publishedAt}</strong></small></p>
+          </div>
+          <a href={NewsUrl} className="btn btn-sm btn-secondary">Read More</a>
+        </div>
+      </div >
+    )
+  }
+}
